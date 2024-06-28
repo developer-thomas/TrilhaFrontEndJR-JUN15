81132fa56143ea719c62bcde514bdd27d71fb300
developer-thomas <andepipous@gmail.com>
2024-06-28 19:45:09 -0400

    style: add an initial spinner

--- trilha-thomas/src/app/app.component.ts
+++ trilha-thomas/src/app/app.component.ts
@@ -1,11 +1,19 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import * as AOS from 'aos';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements AfterViewInit, OnInit {
+  loader: boolean = false;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.loader = true;
+    }, 3000);
+  }
+
   ngAfterViewInit(): void {
     AOS.init();
   }
